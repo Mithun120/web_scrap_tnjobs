aa1ad8e346e07a3c7caaa9048d0c4b8d89c955a1
Mithun M <mithunm.20cse@kongu.edu>
2024-02-22 00:00:43 +0530

    loop

--- task.py
+++ task.py
@@ -15,34 +15,40 @@ extract_list = soup.find('div', class_="col-md-12")
 with open('list.html', 'w') as html_file:
     for i in extract_list:
         html_file.write(i.text)
-
+for i in range(0,351,10):
 # # Find company names using regex
-company_names = re.findall(r'<a\s+style="color:\s*#(?:[0-9a-fA-F]{3}){1,2};?"\s+href="https:\/\/www\.tnprivatejobs\.tn\.gov\.in\/candidate\/Home\/ca_jobfair_single\/\d+">\s*(.*?)\s*<\/a>', html_content)
+    company_names = re.findall(r'<a\s+style="color:\s*#(?:[0-9a-fA-F]{3}){1,2};?"\s+href="https:\/\/www\.tnprivatejobs\.tn\.gov\.in\/candidate\/Home\/ca_jobfair_single\/\d+">\s*(.*?)\s*<\/a>', html_content)
 
 # # Print company names
 # print('company names',company_names)
 
-management_names=re.findall('<a\s+style="color:\s*#\w{6};?"\s+href="https:\/\/www\.tnprivatejobs\.tn\.gov\.in\/Home\/jobsdetails_page\/company\/\d+">\s*(.*?)\s*<\/a>',html_content)
+    management_names=re.findall('<a\s+style="color:\s*#\w{6};?"\s+href="https:\/\/www\.tnprivatejobs\.tn\.gov\.in\/Home\/jobsdetails_page\/company\/\d+">\s*(.*?)\s*<\/a>',html_content)
 # #print management names
 # print('management names',management_names)
 
-work=re.findall('<span\s+style="color:\s*#\w{6};line-height:\s*\d+px;">\s*<i\s+class="fa\s+fa-cogs"\s+aria-hidden="true"\s+style="color:\s*#\w{6};"><\/i>\s*(.*?)\s*<\/span>',html_content)
+    work=re.findall('<span\s+style="color:\s*#\w{6};line-height:\s*\d+px;">\s*<i\s+class="fa\s+fa-cogs"\s+aria-hidden="true"\s+style="color:\s*#\w{6};"><\/i>\s*(.*?)\s*<\/span>',html_content)
 # print('work',work)
 
 # looping
 
 # Determine the length of the lists (assuming they have the same length)
-num_entries = len(company_names)
+    num_entries = len(company_names)
 
 # Write the data to a CSV file
-with open('extracted_data.csv', 'w', newline='', encoding='utf-8') as csv_file:
-    writer = csv.writer(csv_file)
-    writer.writerow(['Company Name', 'Management Name', 'Work'])  # Write header row
-    for i in range(num_entries):
-        writer.writerow([company_names[i], management_names[i], work[i]])
+    with open('extracted_data.csv', 'w', newline='', encoding='utf-8') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(['Company Name', 'Management Name', 'Work'])  # Write header row
+        for i in range(num_entries):
+            writer.writerow([company_names[i], management_names[i], work[i]])
 
 print("CSV file created successfully.")
 
+
+
+###
+
+# jobs=[] for i in range(0,351,10): page_url='https://www.tnprivatejobs.tn.gov.in/Home/jobs/'+str(i) res=requests.get(page_url) soup=BeautifulSoup(res.text,'html.parser') divtag=soup.find_all('li') for li_tag in divtag: h4tag = li_tag.find_all('h4') for h4 in h4tag: jobs.append(h4.text.strip()) print(jobs)
+###
 
 # import requests
 # from bs4 import BeautifulSoup
